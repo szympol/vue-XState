4e46d37a085684501dc7e5e5e9d9c4b77740f12a
Szymon Polanowski <szympol@hotmail.com>
2020-03-14 20:57:24 +0100

    Add XState to Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <textarea v-model="content" rows="10" cols="100" />
+    <br />
+    <br />
+    <button
+      @click="send('SWITCH')"
+    >{{ current.matches('raw') ? 'Show rendered Markdown' : 'Show HTML code' }}</button>
+    <button
+      @click="send('TOGGLE')"
+    >{{ current.matches('hidden') ? 'Show preview' : 'Hide preview' }}</button>
+    <transition name="fade" mode="out-in">
+      <div v-html="rendered" v-if="current.matches('visible.rendered')" key="rendered" />
+      <div v-if="current.matches('visible.raw')" key="raw">
+        <pre>{{ raw }}</pre>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkdownIt from 'markdown-it';
+import { indent } from 'indent.js';
+import { createMachine, State, interpret } from 'xstate';
+
+const md = new MarkdownIt();
+
+const toggleMachine = createMachine({
+  id: 'toggle',
+  initial: 'visible',
+  states: {
+    visible: {
+      on: {
+        TOGGLE: 'hidden'
+      },
+      initial: 'rendered',
+      states: {
+        rendered: {
+          on: {
+            SWITCH: 'raw'
+          }
+        },
+        raw: {
+          on: {
+            SWITCH: 'rendered'
+          }
+        },
+        memo: {
+          type: 'history'
+        }
+      }
+    },
+    hidden: {
+      on: {
+        TOGGLE: 'visible.memo'
+      }
+    }
+  }
+});
+
+const savedState = JSON.parse(localStorage.getItem('state'));
+const previousState = State.create(savedState || toggleMachine.initialState);
+const resolvedState = toggleMachine.resolveState(previousState);
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      content:
+        '# Hello there!\n\n- Type some Markdown on the left\n- See HTML in the right\n- Magic\n\n![An orange jellyfish](https://i.picsum.photos/id/1069/400/250.jpg)',
+      toggleService: interpret(toggleMachine),
+      current: toggleMachine.initialState
+    };
+  },
+  created() {
+    this.toggleService
+      .onTransition(state => {
+        this.current = state;
+
+        try {
+          const state = JSON.stringify(this.current);
+          localStorage.setItem('state', state);
+        } catch {
+          console.error('Local storage is unavailable.');
+        }
+      })
+      .start(resolvedState);
+  },
+  computed: {
+    rendered() {
+      return md.render(this.content);
+    },
+    raw() {
+      return indent.html(this.rendered, {
+        tabString: '  '
+      });
+    }
+  },
+  methods: {
+    send(event) {
+      this.toggleService.send(event);
+    }
   }
-}
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
